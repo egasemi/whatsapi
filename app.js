2fa6c9d69ca3e05f021380988b8d4abe4cce5834
egasemi <emiliano.arregui@gmail.com>
2024-12-11 17:01:23 -0300

    add morgan

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import config from './config/config.js';
 import messageRoutes from './routes/messageRoutes.js';
-import testRoutes from './routes/testRoutes.js'
+import testRoutes from './routes/testRoutes.js';
+import morgan from 'morgan';
 
 const app = express();
 
 // Middleware para parsear JSON
 app.use(express.json());
+app.use(morgan("tiny"))
 
 // Monta las rutas
 app.use('/api/messages', messageRoutes);
